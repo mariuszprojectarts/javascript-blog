f15401f43eb1d44fa6c89ee1e79f34b639c88a45
Mariusz Szleszynski <mariusz.szleszynski@outlook.com>
2020-08-27 21:32:44 +0200

    Add option with class 'active'

--- js/script.js
+++ js/script.js
@@ -7,26 +7,30 @@ document.getElementById('test-button').addEventListener('click', function(){
 */
 
 const titleClickHandler = function(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
-  console.log(event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
 
-const activeLinks = document.querySelectorAll('.titles a.active');
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
-for(let activeLink of activeLinks){
-  activeLink.classList.remove('active');
-}
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
 
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
 
-  /* remove class 'active' from all articles */
+  console.log('clickedElement:', clickedElement);
 
-const activeArticles = document.querySelectorAll('.posts .active');
+  clickedElement.classList.add('active');
 
-for(let activeArticle of activeArticles){
-  activeArticle.classList.remove('active');
-}
+  /* [DONE] remove class 'active' from all articles */
+
+  const activeArticles = document.querySelectorAll('.posts .active');
+
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
 
   /* get 'href' attribute from the clicked link */
 
