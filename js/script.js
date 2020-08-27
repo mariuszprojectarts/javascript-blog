6ac9d01b5871e7911d82a57f87eea9bd6df6c217
Mariusz Szleszynski <mariusz.szleszynski@outlook.com>
2020-08-27 22:27:24 +0200

    Add 'display articles after click'

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ document.getElementById('test-button').addEventListener('click', function(){
 */
 
 const titleClickHandler = function(event){
+  event.preventDefault();                           //zablokowanie domyślnego przewijania strony
   const clickedElement = this;
   console.log('Link was clicked!');
 
@@ -18,7 +19,7 @@ const titleClickHandler = function(event){
     activeLink.classList.remove('active');
   }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
 
   console.log('clickedElement:', clickedElement);
 
@@ -34,9 +35,17 @@ const titleClickHandler = function(event){
 
   /* get 'href' attribute from the clicked link */
 
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
+
   /* find the correct article using the selector (value of 'href' attribute) */
 
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle);
+
   /* add class 'active' to the correct article */
+  
+  targetArticle.classList.add('active');
 
 }
 
